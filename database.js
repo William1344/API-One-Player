6a5bf1a7df9d6240bd282ffddb008b35ac0062a1
William1344 <wb.dom.william@gmail.com>
2022-05-16 06:03:38 -0300

    agora sim sera?

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
-const connection =  new Sequelize('tester', 'root', 'cavalo', {
-  host: 'localhost',
+const connection =  new Sequelize("one_player", "william1344", "cavalocriolo01@", {
+  host: "oneplayermysql.mysql.database.azure.com",
   dialect: 'mysql',
   define: {freezeTableName: true},
 });
